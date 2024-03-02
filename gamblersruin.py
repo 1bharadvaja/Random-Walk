83402e9696de5314bc57c7ef68caeee0828e3609
Aniruddha Bhāradvāja <34083360+1bharadvaja@users.noreply.github.com>
2024-03-01 19:19:41 -0800

    Add files via upload

--- gamblersruin.py
+++ gamblersruin.py
@@ -1,15 +1,18 @@
 import random
 
-total = 0
 
-def randWalk(ruin, win, curr, counter):
-    if (curr == win or curr == ruin):
-        return counter
-    return randWalk(ruin, win, curr + 2*random.randint(0, 1) - 1, counter + 1)
+def returnEV(iterations, ruin, win):
+    total = 0
+    def randWalk(ruin, win, curr, counter):
+        if (curr == win or curr == ruin):
+            return counter
+        return randWalk(ruin, win, curr + 2*random.randint(0, 1) - 1, counter + 1)
 
-i = 0
-while (i <= 10000):
-    total += randWalk(-10, 7, 0, 0)
-    i += 1
+    i = 0
+    while (i <= iterations):
+        total += randWalk(ruin, win, 0, 0)
+        i += 1
 
-print(total/i)
+    return total/iterations #EV comes out to -ruin*win which checks out
+
+returnEV(10000, -10, 7)
